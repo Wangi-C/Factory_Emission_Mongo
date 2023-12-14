fe708d480e371377a05f82d451978e88e85e0920
chowg <devkiwancho@gmail.com>
2023-12-14 20:49:58 +0900

    1. FactoryAddressExcelReader : 공장 (사업장명, 주소) Excel 파일 파싱 2. AddressServiceAPI : 공장 주소 -> 경도,위도 변환 3. Address Enum : 공장 주소 Enum 추가

---
+++ src/main/java/pkg/address/Coordinates.java
@@ -0,0 +1,27 @@
+package pkg.address;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Component
+public class Coordinates {
+    private String name;
+    private String x;
+    private String y;
+
+    @Override
+    public String toString() {
+        return "(" +
+                "\"" + name + '\"' +
+                ", " +  x +
+                ", " + y +
+                ')';
+    }
+}
---
+++ src/main/java/pkg/address/Factory.java
@@ -0,0 +1,13 @@
+package pkg.address;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Factory {
+    private String factoryName;
+    private String factoryAddress;
+}
