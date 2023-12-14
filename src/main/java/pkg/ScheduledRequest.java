d39708c1436e7ce1dfdee656543a66474f3e1b85
chowg <devkiwancho@gmail.com>
2023-12-14 09:57:59 +0900

    SpringBoot_Application & Kafka 연동 1차 테스트 완료 [테스트 내용]  - producer 에서 topic 으로 데이터 전송 및 저장  - terminal 로 해당 topic 내용 확인 : 성공 확인
    [테스트 방법]
     - producer create() 수정 : topic명, 메세지내
     - producerConfig 수정 : BOOTSTRAP_SERVERS_CONFIG, topic 주소 변졍
     - KafkaTest 실행

--- src/main/java/pkg/ScheduledRequest.java
+++ src/main/java/pkg/ScheduledRequest.java
@@ -4,16 +4,20 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 
 @Slf4j
 @EnableScheduling
-@Component
+//@Component
 public class ScheduledRequest {
 
     private final RestTemplate restTemplate;
@@ -25,28 +29,13 @@ public class ScheduledRequest {
         this.mapper = new ObjectMapper();
     }
 
-    @Scheduled(fixedRate = 300000)
+    @Scheduled(fixedRate = 10000)
     public void exec() {
+//        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss");
+//        System.out.println("스케쥴러 테스트 : " + LocalDateTime.now().format(dateTimeFormatter));
         try {
-
-            for(City city: City.values()){
-
-
-                String url = "http://openapi.seoul.go.kr:8088/6d6277526d61736138366457717851/json/citydata_ppltn/1/1/"+city.getCode();
-
-                ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-
-                String jsonResponse = response.getBody();
-
-                JsonNode rootNode = mapper.readTree(jsonResponse);
-
-                ((ObjectNode) rootNode).put("lat", city.getLatitude());
-                ((ObjectNode) rootNode).put("lon", city.getLongitude());
-
-                log.info(rootNode.toString());
-
-            }
-
+            int i = 0;
+            log.info("springboot-elk-01 :: {}", ++i);
         } catch (Exception e) {
             e.printStackTrace();
         }
